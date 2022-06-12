80eb99e3add9dfad13ed07051fa46d71d23008f4
Enes-Ironhack <enes1.koc@epitech.eu>
2022-06-12 18:36:31 +0200

    :tada: [ADD] Infix to RPN, needs some testing

--- back/src/utils/rpn/index.ts
+++ back/src/utils/rpn/index.ts
@@ -44,4 +44,39 @@ export const calc_rpn = (expression: string): number => {
   return stack.pop() as number;
 };
 
-console.log(calc_rpn('1 2 +'));
+type Prio = { [key: string]: number };
+
+const Priority: Prio = { '+': 1, '-': 1, '*': 2, '/': 2, '%': 2 };
+
+// Convet infix to RPN
+export const infix_to_rpn = (expression: string): string => {
+  const stack: string[] = [];
+  const output: string[] = [];
+  const tokens = expression.split(' ');
+
+  for (const token of tokens) {
+    // Append the current token to the end of the postfix expression if it is an operand
+    if (!Number.isNaN(+token)) {
+      output.push(token);
+    } else {
+      // If current operator has lower precedence, first pop operators from the stack to the output
+      while (
+        stack.length > 0 &&
+        Priority[token] <= Priority[stack.slice(-1)[0]]
+      ) {
+        output.push(stack.pop() as string);
+      }
+      // if operator  priority is Higher than the top of the stack, push the token to the stack
+      stack.push(token);
+    }
+  }
+  // while remaining operators in the stack, pop them to the output
+  while (stack.length > 0) {
+    output.push(stack.pop() as string);
+  }
+  return output.join(' ');
+};
+
+console.log(infix_to_rpn('2 + 5 * 3 + 8 * 2'));
+
+console.log(calc_rpn('2 5 3 * + 8 2 * +'));
